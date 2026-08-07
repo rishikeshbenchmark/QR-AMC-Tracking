4a001caab057ef92a59342ee171c9d2934d8e0e7
co2024sohamsagare-cmyk <co2024.soham.sagare@ves.ac.in>
2026-08-07 14:14:51 +0530

    added AMC SUPPLIER

--- server/src/modules/masters/masters.routes.ts
+++ server/src/modules/masters/masters.routes.ts
@@ -2,21 +2,16 @@ import { Router } from 'express';
 
 import { authenticate } from '@/middlewares/auth.middleware';
 
-<<<<<<< HEAD
-import { amcSuppliersRouter } from './amc-suppliers/amc-suppliers.routes';
-=======
 
->>>>>>> 326991d7f55b4f7167ac24c4d9b3cf7cd28f2f94
+import { amcSuppliersRouter } from './amc-suppliers/amc-suppliers.routes';
+
 import { categoriesRouter } from "./categories/categories.routes";
 import { customersRouter } from "./customers/customers.routes";
 import { suppliersRouter } from "./suppliers/suppliers.routes";
 import { makesRouter } from "./makes/makes.routes";
 import { modelsRouter } from "./models/models.routes";
-<<<<<<< HEAD
-=======
-import { amcSuppliersRouter } from './amc-suppliers/amc-suppliers.routes';
->>>>>>> 326991d7f55b4f7167ac24c4d9b3cf7cd28f2f94
 
+import { amcsuppliersRouter } from './amc-suppliers/amc-suppliers.routes';
 
 /**
  * Aggregates the master resources under /masters. `authenticate` runs once here so each resource
@@ -25,10 +20,7 @@ import { amcSuppliersRouter } from './amc-suppliers/amc-suppliers.routes';
 export const mastersRouter = Router();
 
 mastersRouter.use(authenticate);
-<<<<<<< HEAD
 
-=======
->>>>>>> 326991d7f55b4f7167ac24c4d9b3cf7cd28f2f94
 mastersRouter.use('/categories', categoriesRouter);
 mastersRouter.use('/customers', customersRouter);
 
@@ -41,7 +33,4 @@ mastersRouter.use("/customers", customersRouter);
 mastersRouter.use("/suppliers", suppliersRouter);
 mastersRouter.use("/makes", makesRouter);
 mastersRouter.use("/models", modelsRouter);
-<<<<<<< HEAD
-=======
 
->>>>>>> 326991d7f55b4f7167ac24c4d9b3cf7cd28f2f94
